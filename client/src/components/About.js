0899e3f4cb14aec472e7288d5fcf4ce93c15c906
+ <dfarley1@binghamton.edu>
2023-06-05 12:43:31 -0400

    set up routes

---
+++ client/src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+export default function About() {
+
+  return (
+    <>
+    <main>
+        <header>
+
+      <h1>
+       About
+      </h1>
+
+        </header>
+     
+      </main>
+    </>
+  )
+}
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Home from "./Home.js"
+import About from "./About.js"
 function App() {
-  // Code goes here!
+ return (
+    <div className="app">
+      <nav className="navbar">
+        <Link className='link' to='/' exact="true">Home</Link>
+        <br/>
+        <Link className='link' to='/' exact="true">About</Link>
+        <br/>
+      </nav>
+      <Router>
+        <Switch>
+          <Route path="/"><Home /></Route>
+          <Route path="*"><About/></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

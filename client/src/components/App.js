9128cd0f257d8c0608c0cf78ae91d6211575cdb5
+ <dfarley1@binghamton.edu>
2023-06-05 15:07:39 -0400

    dark theme background

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Game from "./Game.js";
 import Navbar from "./Navbar.js";
 import Leaderboard from "./Leaderboard.js";
-
+import "./app.css"
 function App() {
  return (
     <div>

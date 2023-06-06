840ec025bf86407ab1f874a3664505517e7f150a
Yinson Tso <yinson.tso@gmail.com>
2023-06-06 16:53:56 -0400

    game-display

---
+++ client/src/components/Card.js
@@ -0,0 +1,11 @@
+// import { Card } from "semantic-ui-react";
+
+export default function CryptedCard({character}){
+    console.log(character)
+
+    return(
+        <div className="card">
+            <h1>{character}</h1>
+        </div>
+    )
+}
--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,8 +1,14 @@
+// import { Card } from "semantic-ui-react";
+import Card from "./Card";
+
 function Game(){
+    let crypted = 'foodisthegreatestthingknowntomankind'
     return(
         <div className="Game">
             <div className = "Game-Display">
-                <h1>Game-Display</h1>
+                {crypted.split("").map((character) => {
+                    return <Card character = {character}/>
+                })}
             </div>
             <div className = "Game-Keys">
                 <h1>Game Keys</h1>

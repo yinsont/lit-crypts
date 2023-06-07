70b0e25ccfc317030636790c28fcc79f4627d9d5
Yinson Tso <yinson.tso@gmail.com>
2023-06-07 15:17:04 -0400

    Game Display

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,30 +1,44 @@
 // import { Card } from "semantic-ui-react";
 import Card from "./Card";
 import KeyCard from "./KeyCard";
+import { decipheredIt, encoder } from "./Encryption";
 
-function Game(){
-    let sentence = 'food is the greatest thing known to man kind'
-    sentence = sentence.split('')
+function Game() {
+  let sentence = "food is the greatest thing known to man kind";
+  let puzzle = decipheredIt(sentence, encoder("food"));
+  puzzle = sentence.split(" ");
 
-    let keys = [...new Set(sentence)];
-    
-    return(
-        <div className="Game">
-            <div className = "Game-Display">
-                {sentence.map((character) => {
-                    return <Card character = {character}/>
-                })}
-            </div>
-            <div className = "Game-Keys">
-                <form className="Game-Form">
-                    {keys.map((character) => {
-                        return <KeyCard character = {character}/>
-                    })}
-                    <button>Submit</button>
-                </form>
-            </div>
-        </div>
-    )
+  let keys = [...new Set(sentence)];
+  //   let output = puzzle.join("").split(" ");
+  console.log("result", puzzle);
+  let letter = puzzle.map((w) => {
+    return (
+      <div className="word">
+        {w.split("").map((character) => (
+          <Card character={character} />
+        ))}
+      </div>
+    );
+  });
+
+  return (
+    <div className="Game">
+      <div className="Game-Display">
+        {/* {puzzle.map((character) => {
+          return <Card character={character} />;
+        })} */}
+        {letter}
+      </div>
+      <div className="Game-Keys">
+        <form className="Game-Form">
+          {keys.map((character) => {
+            return <KeyCard character={character} />;
+          })}
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Game
+export default Game;

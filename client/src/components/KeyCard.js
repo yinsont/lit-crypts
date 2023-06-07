d0d1664c901eb345b979afbc8ae7a73f886c311d
Yinson Tso <yinson.tso@gmail.com>
2023-06-07 10:20:52 -0400

    display and key generator

--- client/src/components/KeyCard.js
+++ client/src/components/KeyCard.js
@@ -4,10 +4,7 @@ export default function KeyCard( {character} ){
     }
     return(
         <div >
-
             <input className = "KeyCard" placeholder={character.toUpperCase()}></input>
-
-            {/* <h1>{character}</h1> */}
         </div>
     )
 }

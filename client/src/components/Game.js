b5dfe1df377a066b65ce870c1647ab8fc4b32344
Yinson Tso <yinson.tso@gmail.com>
2023-06-07 17:58:43 -0400

    Working on Inputs

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,18 +1,22 @@
-// import { Card } from "semantic-ui-react";
+import {React, useState} from 'react'
 import Card from "./Card";
 import KeyCard from "./KeyCard";
 import StopWatch from "./StopWatch/StopWatch.jsx";
 import Encryption from "./Encryption";
 
 function Game() {
+
+    const [characterKey, setCharacterKey] = useState('')
+    
+    const changeCharacterKey = (e) => setCharacterKey(e.target.value.toUpperCase())
+
   let sentence = "food is the greatest thing known to man kind";
-  sentence = (Encryption('JOHN', 'TAR IS FOUND HERE'))
+  sentence = Encryption("JOHN", "TAR IS FOUND HERE");
 
   let puzzle = sentence.split(" ");
 
-  let keys = [...new Set(sentence)];
-  //   let output = puzzle.join("").split(" ");
-//   console.log("result", puzzle);
+  let keys = [...new Set(sentence)]; //make new array have one of each character
+
   let letter = puzzle.map((w) => {
     return (
       <div className="word">
@@ -22,6 +26,11 @@ function Game() {
       </div>
     );
   });
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("hi");
+  };
 
   return (
     <div className="Game">
@@ -34,10 +43,10 @@ function Game() {
       <div className="Game-Keys">
         <form className="Game-Form">
           {keys.map((character) => {
-            return <KeyCard character={character} />;
+            return <KeyCard character={character} changeCharacterKey = {changeCharacterKey} characterKey={characterKey}/>;
           })}
-          <button>Submit</button>
-          <aside className ="timerStyle">
+          <button onClick={handleClick}>Submit</button>
+          <aside className="timerStyle">
             <StopWatch />
           </aside>
         </form>
--- client/src/components/KeyCard.js
+++ client/src/components/KeyCard.js
@@ -1,10 +1,14 @@
-export default function KeyCard( {character} ){
+import {React, useState} from 'react'
+import ReactDOM from 'react-dom/client'
+
+export default function KeyCard( {characterKey, character, changeCharacterKey} ){
+
     if (character == ' '){
         return null
     }
     return(
         <div >
-            <input className = "KeyCard" placeholder={character.toUpperCase()}></input>
+            <input className = "KeyCard" value = {characterKey} onChange={changeCharacterKey} placeholder={character.toUpperCase()}></input>
         </div>
     )
 }

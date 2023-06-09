9b700f08c589080e1a0a985793c16b6f9754e260
Yinson Tso <yinson.tso@gmail.com>
2023-06-09 13:07:39 -0400

    Working Submit Data

--- client/src/components/GuessForm.js
+++ client/src/components/GuessForm.js
@@ -1,23 +1,33 @@
 import {React, useState} from 'react'
 import ReactDOM from 'react-dom/client'
 
-export default function GuessForm({keySelect, puzzle}){
+export default function GuessForm({puzzle}){
 
     // console.log(guess)
 
     const [guess, setGuess] = useState("");
-
+    const [select, setSelect] = useState('')
+ 
     const handleGuess = (value) => {
       setGuess(value);
     //   console.log(value)
     };
+
+    const handleSelect = (value) => {
+        setSelect(value)
+    }
 
     let new_puzzle = puzzle.join("");
     new_puzzle = new_puzzle.split("");
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log('hi')
+        // console.log('hi')
+        let attempt = {
+            select: select,
+            input: guess,
+        }
+        console.log(attempt)
       }
     
     let keys = [...new Set(new_puzzle)]; //make new array have one of each character
@@ -29,7 +39,7 @@ export default function GuessForm({keySelect, puzzle}){
 
     return(
         <form className="Game-Form" onSubmit = {handleSubmit} > 
-            <select>
+            <select onChange = {e => handleSelect(e.target.value)}>
                 <option></option>
                 {optionCreator}
             </select>

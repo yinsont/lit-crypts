2a7453cd131339e1535335fa89a6addbba1f9482
+ <dfarley1@binghamton.edu>
2023-06-12 18:42:53 -0400

    modify tables

--- server/models.py
+++ server/models.py
@@ -11,9 +11,7 @@ class User(db.Model, SerializerMixin):
     username = db.Column(db.String)
     email = db.Column(db.String)
     password = db.Column(db.String)
-    puzzlescores = db.relationship('Puzzlescore', backref='user', lazy=True)
-
-    serialize_rules = ("-puzzlescores.user", "-puzzlescores.puzzle")
+    score = db.Column(db.Integer)  
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -22,41 +20,24 @@ class Puzzle(db.Model, SerializerMixin):
     __tablename__ = 'puzzles'
 
     id = db.Column(db.Integer, primary_key=True)
-    quote = db.Column(db.String(255))  # New quote field
+    quote = db.Column(db.String(255))  
     created_at = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
-    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now(), nullable=False)
-
-    scores = db.relationship('Puzzlescore', backref='puzzle', lazy=True)  
-
-    serialize_rules = ("-scores.user", "-scores.puzzle")
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now(), nullable=False)  
 
     def __repr__(self):
         return '<Puzzle %r>' % self.id
-
-class Puzzlescore(db.Model, SerializerMixin):
-    __tablename__ = 'puzzlescores'
-
-    id = db.Column(db.Integer, primary_key=True)
-    score = db.Column(db.Integer, nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    puzzle_id = db.Column(db.Integer, db.ForeignKey('puzzles.id'))
-
-    created_at = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
-    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now(), nullable=False)
-
-    serialize_rules = ("-user.puzzlescores", "-puzzle.scores")
-
-    def __repr__(self):
-        return '<Puzzlescore %r>' % self.id
 
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String)
-    username = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', backref=db.backref('messages', lazy=True))
+    score = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
     updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now(), nullable=False)
 
+    serialize_rules = ("-user.messages", "-user.id")
     def __repr__(self):
         return '<Message %r>' % self.body

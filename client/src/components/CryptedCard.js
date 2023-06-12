3a65d7119af1c5a05a981d52325d2079bc3dcf36
Yinson Tso <yinson.tso@gmail.com>
2023-06-11 23:06:53 -0400

    working front-end crypted-card file

--- client/src/components/CryptedCard.js
+++ client/src/components/CryptedCard.js
@@ -1,14 +1,15 @@
-export default function CryptedCard({ character, value }) {
-
+export default function CryptedCard({ character, value, attempts }) {
+  // console.log(value); // Output: "W
   console.log(value)
-  // console.log(key)
-  // || (input.toUpperCase() == value)
+  console.log(attempts)
+  
+
   if (character == " ") {
     return <div className="Empty-Card"></div>;
-  } else if ((character == value)){
+  } else if (character == value.original) {
     return (
       <div className="card-green" value={value}>
-        <h1>{character.toUpperCase()}</h1>
+        <h1>{value.original.toUpperCase()}</h1>
       </div>
     );
   } else {
--- client/src/components/GuessForm.js
+++ client/src/components/GuessForm.js
@@ -1,7 +1,7 @@
 import {React, useState} from 'react'
 import ReactDOM from 'react-dom/client'
 
-export default function GuessForm({puzzle}){
+export default function GuessForm({puzzle, handleAttempts}){
 
     // console.log(guess)
 
@@ -9,8 +9,9 @@ export default function GuessForm({puzzle}){
     const [select, setSelect] = useState('')
  
     const handleGuess = (value) => {
+        // value.preventDefault()
+      console.log(value)
       setGuess(value);
-    //   console.log(value)
     };
 
     const handleSelect = (value) => {
@@ -27,7 +28,7 @@ export default function GuessForm({puzzle}){
             select: select,
             input: guess,
         }
-        console.log(attempt)
+        handleAttempts(attempt)
       }
     
     let keys = [...new Set(new_puzzle)]; //make new array have one of each character
